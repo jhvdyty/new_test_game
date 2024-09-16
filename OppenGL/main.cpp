34d969fe0b623b140d7d411c9ccb5539df35d904
Vlodimir <asakira884@gmail.com>
2024-09-16 21:54:43 +0300

    Добавьте файлы проекта.

---
+++ OppenGL/main.cpp
@@ -0,0 +1,220 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <random>
+
+#include "shader.h"
+#include "stb_image.h"
+
+float quadLeft = -0.15f;
+float quadRight = 0.15f;
+float quadTop = 0.15f;
+float quadBottom = -0.15f;
+
+double random_number_y;
+double random_number_x;
+
+float x_ran = 0.0f;
+float y_ran = 0.0f;
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+
+        // Преобразование координат экрана в координаты OpenGL
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        float x = (2.0f * xpos) / width - 1.0f;
+        float y = 1.0f - (2.0f * ypos) / height;
+
+        if (x >= quadLeft + x_ran && x <= quadRight + x_ran && y >= quadBottom+ y_ran && y <= quadTop+ y_ran)
+        {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> dis(-0.5, 0.5);
+
+            x_ran = dis(gen);
+            y_ran = dis(gen);
+        }
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+GLfloat mixValue = 0.2f;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        mixValue += 0.01f;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        mixValue -= 0.01f;
+}
+
+unsigned int loadTexture(const char* path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+
+    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+
+}
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window!", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    Shader ourShader("C:/Users/Admin/Downloads/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/OppenGL/vertex.glsl", "C:/Users/Admin/Downloads/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/OppenGL/fragment.glsl");
+
+    unsigned int texture1 = loadTexture("C:/Users/Admin/Downloads/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/opengl/texture/brickwall.jpg");
+    unsigned int texture2 = loadTexture("C:/Users/Admin/Downloads/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/my_en-1880bb6b480cd15432110ac956c122b6d338aac9/opengl/texture/brickwall_normal.jpg");
+
+
+    GLfloat vertices[] = {
+        // Positions          // Colors           // Texture Coords
+         0.15f,  0.15f, 0.0f,   0.5f, 0.0f, 0.0f,   0.5f, 0.5f, // Top Righ // Top Right
+         0.15f, -0.15f, 0.0f,   0.0f, 0.5f, 0.0f,   0.5f, 0.0f, // Bottom Right
+        -0.15f, -0.15f, 0.0f,   0.0f, 0.0f, 0.5f,   0.0f, 0.0f, // Bottom Left
+        -0.15f,  0.15f, 0.0f,   0.5f, 0.5f, 0.0f,   0.0f, 0.5f  // Top Left 
+    };
+
+    GLuint indices[] = {
+        0,1,3,
+        1,2,3
+    };
+
+    GLuint VBO, VAO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+
+
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GLFW_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    //glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+ 
+
+    while (!glfwWindowShouldClose(window)) {
+        // ... Обработка событий (если необходимо) ...
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // ... Команды отрисовки ...
+
+        //GLfloat timeValue = glfwGetTime();
+        //GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+        //GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);
+
+        glUniform1f(glGetUniformLocation(ourShader.Program, "mixValue"), mixValue);
+
+        glUniform1f(glGetUniformLocation(ourShader.Program, "x_ran"), x_ran); 
+        glUniform1f(glGetUniformLocation(ourShader.Program, "y_ran"), y_ran); 
+
+        processInput(window);
+        glfwSetMouseButtonCallback(window, mouse_button_callback); 
+
+
+
+        
+
+        ourShader.Use();
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 6, 0);
+        //vertexColorLocation = glGetUniformLocation(secondShaderProgram, "ourColor");
+        //glUniform4f(vertexColorLocation, 0.5f, greenValue, 0.7f, 1.0f);
+
+        glBindVertexArray(0);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glfwTerminate();
+    return 0;
+}
